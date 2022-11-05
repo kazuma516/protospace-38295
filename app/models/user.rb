bcc01ecc794e142b25917051f42e25b0088b7f52
kazuma516 <kizuma.9516@gmail.com>
2022-11-05 15:43:12 +0900

    あ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :prototype
+  has_many :prototypes
   has_many :comments
 
   validates :name, presence: true
